81c2a2c69fa4cac5edb99a0b05d49c1e8cfd9d30
Ramiro Nilson Barros <87489102+RamiroNB@users.noreply.github.com>
2024-11-19 23:18:46 +0000

    Add initial version of sched-profiler

--- qemu-up.sh
+++ qemu-up.sh
@@ -9,6 +9,15 @@
 #	--append "console=ttyS0 root=/dev/sda" 
 
 
+# export LINUX_OVERRIDE_SRCDIR=./kernel/linux-4.13.9
+
+# qemu-system-i386 --kernel output/images/bzImage --hda output/images/rootfs.ext2 --hdb sdb.bin --nographic --append "console=ttyS0 root=/dev/sda"
+
 export LINUX_OVERRIDE_SRCDIR=./kernel/linux-4.13.9
 
-qemu-system-i386 --kernel output/images/bzImage --hda output/images/rootfs.ext2 --hdb sdb.bin --nographic --append "console=ttyS0 root=/dev/sda"
+qemu-system-i386 --kernel output/images/bzImage --hda output/images/rootfs.ext2 --nographic --append "console=ttyS0 root=/dev/sda"
+
+
+
+# ./output/host/bin/i686-buildroot-linux-gnu-gcc -o ./output/target/bin/sched_profiler ./custom-scripts/sched_profiler/sched.c
+# make
